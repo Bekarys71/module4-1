a44c72b8ad72b7fd7eb0add985ed4d0eea85893b
Bekarys71 <Beybarys.imashev@mail.ru>
2024-12-14 14:31:42 +0600

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,10 @@
+from fake_math import divide as d1
+from true_math import divide as d2
+print('result1=', d1(69 , 3))
+print('result2=',d1(3,0))
+print('result3=',d2(49, 7))
+print('result4=',d2(15,0))
+
+
+
+
